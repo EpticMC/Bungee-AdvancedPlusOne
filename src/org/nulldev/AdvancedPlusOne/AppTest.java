06a1be4380f8a8ff535872d80d81a13752e1307c
NullDev (Shadow) <NL-Dev@yandex.com>
2017-09-08 14:15:19 +0200

    Init

---
+++ src/org/nulldev/AdvancedPlusOne/AppTest.java
@@ -0,0 +1,13 @@
+package org.nulldev.AdvancedPlusOne;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest
+extends TestCase {
+    public AppTest(String testName) { super(testName); }
+    public static Test suite() { return new TestSuite(AppTest.class); }
+    public void testApp() { AppTest.assertTrue((boolean)true); }
+}
+
